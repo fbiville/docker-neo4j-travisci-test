9fdbb7e6bae450faddc6b127a57c4b727d04a223
Florent Biville <florent.biville@gmail.com>
2017-09-10 01:27:00 +0200

    Add missing version to `docker run` command

--- _scripts/run.sh
+++ _scripts/run.sh
@@ -2,7 +2,7 @@
 set -e
 if [ "$WITH_DOCKER" = true ] ; then
     docker pull neo4j:${NEO_VERSION}
-    docker run --detach --publish=7474:7474 --volume=$HOME/neo4j/data:/data --env=NEO4J_AUTH=neo4j/j4oen neo4j
+    docker run --detach --publish=7474:7474 --volume=$HOME/neo4j/data:/data --env=NEO4J_AUTH=neo4j/j4oen neo4j:${NEO_VERSION}
     mvn -T4 clean test -Dneo4j.version=${NEO_VERSION}
 else
     echo 'Skipping older Neo4j versions...'
